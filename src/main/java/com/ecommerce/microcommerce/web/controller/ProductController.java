84eed352664d59cef83235e32b86084c2172ae8d
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-10-27 12:07:44 +0200

    Added POST request (test with postman)

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,9 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.dao.ProductDao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,11 @@ public class ProductController {
     @GetMapping(value = "/Produits/{id}")
     public Product afficherUnProduit(@PathVariable int id) {
         return productDao.findById(id);
+    }
+
+    @PostMapping(value = "/Produits")
+    public void ajouterProduit(@RequestBody Product product){
+        productDao.save(product);
     }
 }
 
